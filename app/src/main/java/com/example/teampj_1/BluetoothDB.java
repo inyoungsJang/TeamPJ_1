c01104ba9330c2be48e3a93181836dce96069cb1
willlead <willlead@naver.com>
2020-02-03 12:01:29 +0900

    db 클래스 수정

--- app/src/main/java/com/example/teampj_1/BluetoothDB.java
+++ app/src/main/java/com/example/teampj_1/BluetoothDB.java
@@ -10,35 +10,33 @@ import android.os.Bundle;
 
 public class BluetoothDB extends AppCompatActivity {
     SQLiteDatabase sqlDB;
-    bluetoothUserDB bluetoothUserDB;
+    BluetoothUserDB bluetoothUserDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_db);
 
-           bluetoothUserDB = new bluetoothUserDB(getApplicationContext());
-
-
+           bluetoothUserDB = new BluetoothUserDB(getApplicationContext());
     }
+}
 
-    class bluetoothUserDB extends SQLiteOpenHelper {
-        public bluetoothUserDB(@Nullable Context context) {
-            super(context, "bluetoothUserDB", null, 1); //DB생성
-        }
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE bluetoothUserTBL (id TEXT(20) NOT NULL PRIMARY KEY, password TEXT(20) NOT NULL, name TEXT(50) NOT NULL, rfid TEXT(50), email TEXT(10) NOT NULL);"); //bluetoothUserTBL Table생성
-        }
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {        }
-        @Override
-        public SQLiteDatabase getWritableDatabase() {
-            return super.getWritableDatabase();
-        }
-        @Override
-        public SQLiteDatabase getReadableDatabase() {
-            return super.getReadableDatabase();
-        }
+class BluetoothUserDB extends SQLiteOpenHelper {
+    public BluetoothUserDB(@Nullable Context context) {
+        super(context, "bluetoothUserDB", null, 1); //DB생성
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE bluetoothUserTBL (id TEXT(20) NOT NULL PRIMARY KEY, password TEXT(20) NOT NULL, name TEXT(50) NOT NULL, rfid TEXT(50), email TEXT(10) NOT NULL);"); //bluetoothUserTBL Table생성
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {        }
+    @Override
+    public SQLiteDatabase getWritableDatabase() {
+        return super.getWritableDatabase();
+    }
+    @Override
+    public SQLiteDatabase getReadableDatabase() {
+        return super.getReadableDatabase();
     }
 }
