d7b30197770f44fe80fe658283702dc14d16cb7f
inyoungsJang <zxzxdlsdud@naver.com>
2020-02-25 14:07:43 +0900

    TODO : 도움말해야함 /현경쓰 ㅎㅇㅌ //아이디,비밀번호찾기 해야함 //비밀번호 찾기 (오류)수정 요망

--- app/src/main/java/com/example/teampj_1/FindAccountActivity.java
+++ app/src/main/java/com/example/teampj_1/FindAccountActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Editable;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FindAccountActivity extends AppCompatActivity {
     TextView tvLostID;
@@ -22,9 +24,11 @@ public class FindAccountActivity extends AppCompatActivity {
     Button btnAccountCheck;
 
     String strName, strId;
+    String cId, cName, cPassword;
 
     BluetoothDB btDB;
     SQLiteDatabase sqlDB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +48,12 @@ public class FindAccountActivity extends AppCompatActivity {
         edtName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+                strName = edtName.getText().toString();
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                strName = edtName.getText().toString();
+
             }
 
             @Override
@@ -57,6 +61,48 @@ public class FindAccountActivity extends AppCompatActivity {
             }
         });
 
+        edtId.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+               strId= edtId.getText().toString();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        btDB = new BluetoothDB(this);
+        sqlDB = btDB.getReadableDatabase();
+        Cursor cursor = sqlDB.rawQuery("SELECT id,name,password FROM bluetoothUserTBL WHERE name='"+strName+"';", null);
+        if (cursor.moveToFirst()) {
+            cId = cursor.getString(0);
+            cName = cursor.getString(1);
+            cPassword = cursor.getString(2);
+
+        } else {
+            Toast.makeText(getApplicationContext(), "가입된 회원정보가 없습니다.", Toast.LENGTH_SHORT).show();
+        }
+
+        btnAccountCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (strId.equals("") && strName.equals("")) {
+                    Toast.makeText(FindAccountActivity.this, "빈칸을 채워주세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (strName.equals(cName) && strId.equals(cId)) { //
+                        tvShowPassword.setText(cPassword);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "없는 회원정보입니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
         //마지막 저장값 가져와야함.
         tvLostID.setOnClickListener(new View.OnClickListener() {
